89ff87113b4ddc4e8d2164bb8dc59c8e4e40fa47
Eren Gun <70534390+Erengun@users.noreply.github.com>
2022-01-30 20:10:10 +0300

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egun <egun@student.42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/15 18:24:32 by egun              #+#    #+#             */
+/*   Updated: 2022/01/30 20:09:17 by egun             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	check_error(int a, char *str, char *buffer)
+{
+	if (a < 0)
+	{
+		free (str);
+		free (buffer);
+		return (0);
+	}
+	return (1);
+}
+
+char	*getstr(int fd, char *str)
+{
+	int		a;
+	char	*buffer;
+	char	*s;
+
+	if (!str)
+		str = ft_strdup("");
+	a = 1;
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	while ((a != 0) && !(ft_strchr(str, '\n')))
+	{
+		a = read(fd, buffer, BUFFER_SIZE);
+		if (a == 0)
+			break ;
+		if (!check_error(a, str, buffer))
+			return (NULL);
+		buffer[a] = '\0';
+		s = ft_strjoin(str, buffer);
+		free(str);
+		str = s;
+	}
+	free(buffer);
+	return (str);
+}
+
+char	*getnewline(char *str)
+{
+	char	*ptr;
+	int		len;
+
+	if (!str)
+		return (NULL);
+	if (!*str)
+		return (NULL);
+	ptr = ft_strchr(str, '\n');
+	if (!ptr)
+		return (ft_strdup(str));
+	ptr++;
+	if (!*ptr)
+		return (ft_strdup(str));
+	len = ft_strlen(str) - ft_strlen(ptr);
+	return (ft_substr(str, 0, len));
+}
+
+char	*getremain(char *str)
+{
+	char	*ptr;
+
+	ptr = ft_strchr(str, '\n');
+	if (!ptr)
+	{
+		free(str);
+		return (NULL);
+	}
+	ptr++;
+	if (!(*ptr))
+	{
+		free(str);
+		return (NULL);
+	}
+	ptr = ft_strdup(ptr);
+	free (str);
+	return (ptr);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str[1024];
+	char		*line;
+
+	str[fd] = getstr(fd, str[fd]);
+	if (!str[fd])
+		return (NULL);
+	line = getnewline(str[fd]);
+	str[fd] = getremain(str[fd]);
+	return (line);
+}
+
+#include <fcntl.h>
+#include <stdio.h>
+
+int   main(int argc, char **argv)
+{
+  int   fd;
+  int	fd2;
+  char *c;
+  char *x;
+
+  fd = open(argv[1], O_RDWR);
+  c = get_next_line(fd);
+  while(c)
+  {
+    printf("%s",c);
+	printf("%s",x);
+    free(c);
+    c = get_next_line(fd);
+  }
+  // For checking leaks
+  //system("leaks a.out");
+}
---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egun <egun@student.42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 16:32:17 by egun              #+#    #+#             */
+/*   Updated: 2022/01/29 20:20:06 by egun             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5000
+# endif
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*getstr(int fd, char *str);
+char	*getnewline(char *str);
+char	*getremain(char *str);
+char	*get_next_line(int fd);
+
+#endif
