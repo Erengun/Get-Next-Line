c7b12a2068bdba5777889a281c3650df1d4cfa88
Eren Gun <70534390+Erengun@users.noreply.github.com>
2022-01-31 16:49:28 +0300

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -10,15 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5000
+#  define BUFFER_SIZE 31
 # endif
 
 char	*ft_strjoin(char const *s1, char const *s2);
